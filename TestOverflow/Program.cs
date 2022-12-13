ae6cd686d2bf22d74812d2647f98c12c4f665727
user <user@ZENOBEAST>
2022-12-13 23:49:35 +0545

    Add project files.

---
+++ TestOverflow/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //checked keyword: with this overflow will not happen at runtime and Exception will be thrown and program will crash unless u
+            //handle exception.
+            checked
+            {
+                byte number = 255;
+                number += 1;//0
+
+            }
+        }
+
+    }
+}
